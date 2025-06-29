713ba79b84040188724b1da958f686d035affb4d
khouloudBoussaha <khouloud.boussaha@esprit.tn>
2025-06-29 20:24:06 +0200

    commitmodel55

--- Continuix/src/main/java/tn/sopra/continuix/config/SecurityConfig.java
+++ Continuix/src/main/java/tn/sopra/continuix/config/SecurityConfig.java
@@ -75,7 +75,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:4200",
+                "https://projetpfe-angular.onrender.com"
+        ));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
